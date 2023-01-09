be8ef11cb4a7d0edbf484be7f32d0ddd811aef7c
Philip F <theaavagames@gmail.com>
2023-01-08 21:42:13 -0400

    added .pdx to gitignore, created actor template

---
+++ TextWorld/source/actorTemplate.lua
@@ -0,0 +1,15 @@
+import "global"
+import "actor"
+
+class("a").extends(actor)
+
+function a:init(theWorld, x, y)
+    a.super.init(self, theWorld, x, y)
+    self.char = "M"
+    self.name = "Wall"
+    self.description = "A cold wall."
+end
+
+function a:update()
+    a.super.update(self)
+end
